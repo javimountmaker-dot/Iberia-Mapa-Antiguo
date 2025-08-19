39493d579d16c7806124a687721a4004e15c91c4
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-19 16:16:54 -0700

    feat(frontend): add App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id="app">
+      <h1>Ancient Maps - MVP Iberia</h1>
+      <p>PWA scaffolding. Map and components will be added soon.</p>
+    </div>
+  );
+}
+
+export default App;
