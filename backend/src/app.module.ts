d9dad5bbdca34a0cb94612c466d6f4201b904f41
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-20 12:28:57 -0700

    feat(backend): register places module in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlacesController } from './places.controller';
+import { PlacesService } from './places.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PlacesController],
+  providers: [AppService, PlacesService],
 })
 export class AppModule {}
