d6fb1a685fd3fd1d1bc0246cb3d1d50651016ffd
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-20 12:41:10 -0700

    feat(backend): add rivers service

---
+++ backend/src/rivers.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RiversService {
+  // TODO: Implement retrieval of rivers by year
+  findAll(year?: number) {
+    return [];
+  }
+}
