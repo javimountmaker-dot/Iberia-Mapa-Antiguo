18fedb21ab9df5353da4bc1b89f4b2a293606ba4
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-20 12:44:04 -0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlacesController } from './places.controller';
 import { PlacesService } from './places.service';
+import { RoadsController } from './roads.controller';
+import { RoadsService } from './roads.service';
+import { RiversController } from './rivers.controller';
+import { RiversService } from './rivers.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PlacesController],
-  providers: [AppService, PlacesService],
+  controllers: [AppController, PlacesController, RoadsController, RiversController],
+  providers: [AppService, PlacesService, RoadsService, RiversService],
 })
 export class AppModule {}
