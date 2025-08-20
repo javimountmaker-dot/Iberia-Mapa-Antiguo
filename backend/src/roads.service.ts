9cfdbfca8d161a68fd1954d000093e2a1316821c
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-20 12:37:27 -0700

    feat(backend): add roads service

---
+++ backend/src/roads.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RoadsService {
+  // TODO: Implement retrieval of roads by year
+  findAll(year?: number) {
+    return [];
+  }
+}
