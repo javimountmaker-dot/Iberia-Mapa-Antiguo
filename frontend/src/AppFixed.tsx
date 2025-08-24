95737b25adaa54e16e6310f2251744b0e98f3f69
javimountmaker-dot <javimountmaker@gmail.com>
2025-08-24 02:14:40 -0700

    feat(frontend): add AppFixed component

---
+++ frontend/src/AppFixed.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Map from './components/Map';
+import TimeSlider from './components/TimeSliderFixed';
+import SearchBar from './components/SearchBar';
+
+function AppFixed() {
+  const [year, setYear] = useState(-500);
+  const [query, setQuery] = useState('');
+
+  return (
+    <div id="app" style={{ height: '100vh', width: '100%' }}>
+      <h1>Ancient Maps - MVP Iberia</h1>
+      <SearchBar value={query} onChange={setQuery} />
+      <TimeSlider year={year} onChange={setYear} />
+      <Map />
+    </div>
+  );
+}
+
+export default AppFixed;
